3992333d51f7502e32c5dffdcaf75c547749711b
no-relation <eddiec76@gmail.com>
2018-11-19 12:52:13 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file.
+function lowerCaseDrivers(drivers){
+    return drivers.map(
+        function(dr){
+            return dr.toLowerCase()
+        }
+    )
+};
+
+function nameToAttributes(drivers){
+    return drivers.map(
+        function(dr){
+            const spaceIndex = dr.indexOf(" ")
+            return { firstName: dr.slice(0, spaceIndex), lastName: dr.slice(spaceIndex+1) }
+        }
+    )
+};
+
+function attributesToPhrase(drivers){
+    return drivers.map(
+        function(dr){
+            return `${dr.name} is from ${dr.hometown}`
+        }
+    )
+};
